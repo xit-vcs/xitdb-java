5be9ae351ef420f46c6781ffab6a7253a17da979
radar roark <122068506+xeubie@users.noreply.github.com>
2026-09-11 07:15:22 -0400

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "io.github.radarroark"
-version = "0.38.0"
+version = "0.39.0"
 description = "An immutable database"
 java.sourceCompatibility = JavaVersion.VERSION_17
 java.targetCompatibility = JavaVersion.VERSION_17
